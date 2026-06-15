781056bf6abc3e6b2e6e529a07a54c335f81b5b7
abhinav-shukla0022 <abhinav.shukla22k@gmail.com>
2026-06-15 17:34:27 +0530

    change gemini model

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ console.log("KEY PREFIX:", process.env.GEMINI_API_KEY?.substring(0, 8));
 // ─── Gemini Setup ─────────────────────────────────────────────────────────────
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.5-flash"
+  model: "gemini-2.5-flash-lite"
 });
 
 const callGeminiWithRetry = async (prompt, maxRetries = 3) => {
